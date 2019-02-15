d2450b0bb22f1f8876e231a2cab73033d7b19817
Tung Nguyen <tongueroo@gmail.com>
2019-02-15 07:01:15 +0000

    set root_directory to nil

--- lib/airbrake_jets/turbine.rb
+++ lib/airbrake_jets/turbine.rb
@@ -8,7 +8,7 @@ module AirbrakeJets
         c.project_key = ENV["AIRBRAKE_PROJECT_KEY"]
         c.environment = Jets.env.to_s
         c.ignore_environments = %w(development test cucumber)
-        c.root_directory = Jets.root.to_s
+        c.root_directory = nil # to bypass Airbrake::Filters
       end
     end
 
